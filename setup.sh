0672044f514c52208e97c0c7acccbcbd0ea78f00
Thomas Stitt <tomstit@gmail.com>
2015-07-01 18:17:27 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,3 @@ export SPOTIFY_CLIENT_ID='d6c0a432650f4184ac886377a5255014'
 export SPOTIFY_SECRET='b7697eb6d6304a17b47302ede0188532'
 export CROWDIFY_URL_BASE='http://crowdify.duckdns.org'
 export CROWDIFY_PUBLIC_DIR='public'
-sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8080
